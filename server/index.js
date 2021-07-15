23cfe5c3c10158d84df163109126d190bfebe4a0
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-07-15 11:17:26 -0600

    fixing error

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'../index.html'))
 })
 
-const port = proces.env.PORT || 4005
+const port = process.env.PORT || 4005
 
 app.listen(port,() => {
     console.log('Server listening on 4005')
